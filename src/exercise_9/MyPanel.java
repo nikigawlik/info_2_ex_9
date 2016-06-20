1561466debf0dff8675d230a3f3723d14123eb7a
Nils <nilsgawlik@gmx.de>
2016-06-20 18:31:19 +0200

    actually added comments :P

--- src/exercise_9/MyPanel.java
+++ src/exercise_9/MyPanel.java
@@ -7,8 +7,14 @@ import java.awt.Graphics;
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
+/*
+ * Code partially copied from https://docs.oracle.com/javase/tutorial/uiswing/painting/step1.html
+ *
+ */
+
 class MyPanel extends JPanel {
 	
+	//unneccesary custom class, could have used java.awt.Point instead
 	private class Point{
 		public int x;
 		public int y;
@@ -16,7 +22,9 @@ class MyPanel extends JPanel {
 			this.x = x;
 			this.y = y;
 		}
-		
+		/*
+		 * calculates the middle point between this point and the provided point.
+		 */
 		public Point midPoint(Point otherPoint)
 		{
 			return new Point((x + otherPoint.x) / 2, (y + otherPoint.y) / 2);
@@ -28,10 +36,8 @@ class MyPanel extends JPanel {
 		}
 	}
 
-    /**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
+	private final boolean FILLED = true;
 
 	public MyPanel() {
         setBorder(BorderFactory.createLineBorder(Color.black));
@@ -51,48 +57,72 @@ class MyPanel extends JPanel {
         int h = this.getHeight()-64;
         w = Math.min(w, h);
 
+        /*
+         * calculate the points of the outer triangle relative to 
+         * the panel size
+         * 0.86603 = sqrt(3)/2 
+         * This makes the triangle equilateral (-> pythagorean theorem)
+         */
         Point p1 = new Point(32+w/2, 32+w-(int)(w*0.86603));
         Point p2 = new Point(32, 32+w);
         Point p3 = new Point(32+w, 32+w);
         
-        
+        //start the recursion which draws the triangle
         drawSierpinskiTriangle(g, p1, p2, p3, 8);
         
     }
     
+    /*
+     * draws a sierpinski triangle through three points
+     */
     private void drawSierpinskiTriangle(Graphics g, Point p1, Point p2, Point p3, int numberOfIterations)
     {
-    	drawTriangle(g, p1, p2, p3);
+    	//draw outer triangle
+    	drawTriangle(g, p1, p2, p3, FILLED);
+    	//draw inner structure, start recursion
         drawInnerSierpinskiTriangle(g, p1, p2, p3, numberOfIterations);
     }
     
+    /*
+     * recursive method
+     * draws the inner triangles of a sub-triangle in a 
+     * sierpinski triangle
+     */
     private void drawInnerSierpinskiTriangle(Graphics g, Point p1, Point p2, Point p3, int numberOfIterations)
     {
+    	//termination
     	if(numberOfIterations < 0)
     		return;
 
+    	//rainbow colors through the iterations
 		g.setColor(Color.getHSBColor((numberOfIterations%8)/8.0F, 1.0F, 1.0F));
     	
+		//calculate the inner triangle points
     	Point p12 = p1.midPoint(p2);
     	Point p23 = p2.midPoint(p3);
     	Point p31 = p3.midPoint(p1);
     	
-    	drawTriangle(g, p12, p23, p31);
+    	//draw inner triangle
+    	drawTriangle(g, p12, p23, p31, FILLED);
 
+    	//do the same for the 3 sub-triangles (recursion)
     	drawInnerSierpinskiTriangle(g, p1, p12, p31, numberOfIterations - 1);
     	drawInnerSierpinskiTriangle(g, p12, p2, p23, numberOfIterations - 1);
     	drawInnerSierpinskiTriangle(g, p31, p23, p3, numberOfIterations - 1);
     }
 
-	private void drawTriangle(Graphics g, Point p1, Point p2, Point p3) {
-//		g.drawLine(p1.x, p1.y, p2.x, p2.y);
-//		g.drawLine(p2.x, p2.y, p3.x, p3.y);
-//		g.drawLine(p3.x, p3.y, p1.x, p1.y);
+    /*
+     * draws a triangle
+     */
+	private void drawTriangle(Graphics g, Point p1, Point p2, Point p3, boolean filled) {
 		
 		int[] xs = {p1.x, p2.x, p3.x};
 		int[] ys = {p1.y, p2.y, p3.y};
 		
-		g.fillPolygon(xs, ys, 3);
+		if(filled)
+			g.fillPolygon(xs, ys, 3);
+		else
+			g.drawPolygon(xs, ys, 3);
 	}  
 	
 }
--- src/exercise_9/TriangleDemo.java
+++ src/exercise_9/TriangleDemo.java
@@ -3,6 +3,10 @@ package exercise_9;
 import javax.swing.SwingUtilities;
 import javax.swing.JFrame;
 
+/*
+ * Code copied from https://docs.oracle.com/javase/tutorial/uiswing/painting/step1.html
+ */
+
 public class TriangleDemo {
     
     public static void main(String[] args) {
@@ -16,7 +20,7 @@ public class TriangleDemo {
     private static void createAndShowGUI() {
         System.out.println("Created GUI on EDT? "+
                 SwingUtilities.isEventDispatchThread());
-        JFrame f = new JFrame("Swing Paint Demo");
+        JFrame f = new JFrame("Sierpinski Triangle");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         f.add(new MyPanel());
